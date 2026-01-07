dd7000097b131550b6ae3e99ae50716b38656ee9
PukarP <105930060+PukarP@users.noreply.github.com>
2026-01-07 17:05:14 +0545

    The database connection is now done>>:)

--- app/app.py
+++ app/app.py
@@ -1,51 +1,13 @@
 from fastapi import FastAPI,HTTPException
 from app.schemas import CreatePost
-app = FastAPI()
+from app.database import Post,create_db_and_tables,get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-thePosts = {
-    1:{
-        "title":"Some shit",
-        "content":"This is some shit.",
-        "author":"Pukar Pandey"
-    },
-    2:{
-        "title":"Some shit",
-        "content":"This is some shit.",
-        "author":"Pukar Pandey"
-    },
-    3:{
-        "title":"Some shit",
-        "content":"This is some shit.",
-        "author":"Pukar Pandey"
-    },
-    4:{
-        "title":"Some shit",
-        "content":"This is some shit.",
-        "author":"Pukar Pandey"
-    },
-    5:{
-        "title":"Some shit",
-        "content":"This is some shit.",
-        "author":"Pukar Pandey"
-    }
-}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
-@app.get("/posts")
-def getAllPosts():
-
-    return thePosts
-
-#this is for like a particular post you click on
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    if id not in thePosts:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return thePosts.get(id)
-
-@app.post("/posts")
-def createPost(post: CreatePost):
-    newPost = {"title": post.title,"content": post.content,"author": post.author}
-    thePosts[max(thePosts.keys())+1] = newPost
-    return {"message": "New thingy added successfully"}
+app = FastAPI(lifespan=lifespan)
 
